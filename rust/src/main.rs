0290302c949ef4c40f2566fb2af69d1a5fa9cb90
juju <gliheng@gmail.com>
2019-05-10 14:33:12 +0800

    merge 3.0 changes

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,19 +4,12 @@
 mod calc_channel;
 mod msg_stream_channel;
 
-use std::{
-    env,
-    path::PathBuf,
-};
-use log::{
-    info,
-};
-use env_logger;
-use flutter_engine::{
-    FlutterEngineArgs,
-    FlutterEngine,
-    WindowMode,
-};
+use std::{env, path::PathBuf};
+
+use fern::colors::{Color, ColoredLevelConfig};
+use flutter_engine::DesktopWindowState;
+use flutter_plugins::{dialog, window};
+use log::info;
 
 #[cfg(target_os = "macos")]
 use core_foundation::bundle;
@@ -24,51 +17,102 @@ use core_foundation::bundle;
 #[cfg(target_os = "macos")]
 fn get_res_dir() -> PathBuf {
     let bd = bundle::CFBundle::main_bundle();
-    let exe = bd.executable_url().expect("Cannot get executable dir").to_path().expect("to_path error");
+    let exe = bd
+        .executable_url()
+        .expect("Cannot get executable dir")
+        .to_path()
+        .expect("to_path error");
     exe.parent().unwrap().parent().unwrap().join("Resources")
 }
 
 #[cfg(not(target_os = "macos"))]
 fn get_res_dir() -> PathBuf {
-    env::current_exe().expect("Cannot get application dir")
-        .parent().expect("Cannot get application dir")
+    env::current_exe()
+        .expect("Cannot get application dir")
+        .parent()
+        .expect("Cannot get application dir")
         .to_path_buf()
 }
 
 fn main() {
-    env_logger::init();
-    flutter_engine::init();
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::White)
+        .trace(Color::BrightBlack);
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Debug)
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}][{}] {}\x1B[0m",
+                format_args!("\x1B[{}m", colors.get_color(&record.level()).to_fg_str()),
+                chrono::Local::now().format("%H:%M:%S"),
+                record.level(),
+                record.target(),
+                message
+            ))
+        })
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
 
     let (assets_path, icu_data_path) = match env::var("CARGO_MANIFEST_DIR") {
         Ok(proj_dir) => {
             info!("Running inside cargo project");
             let proj_dir = PathBuf::from(&proj_dir);
             (
-                proj_dir.parent().unwrap().join("build").join("flutter_assets"),
+                proj_dir
+                    .parent()
+                    .unwrap()
+                    .join("build")
+                    .join("flutter_assets"),
                 proj_dir.join("assets/icudtl.dat"),
             )
-        },
+        }
         Err(_) => {
             let res = get_res_dir();
-            (
-                res.join("flutter_assets"),
-                res.join("icudtl.dat"),
-            )
-        },
+            (res.join("flutter_assets"), res.join("icudtl.dat"))
+        }
     };
 
-    let args = FlutterEngineArgs {
-        assets_path: assets_path.to_string_lossy().into_owned(),
-        icu_data_path: icu_data_path.to_string_lossy().into_owned(),
-        title: String::from("Flutter Demo"),
-        width: 1800,
-        height: 1200,
-        window_mode: WindowMode::Windowed,
-        ..Default::default()
-    };
+    let mut engine = flutter_engine::init().unwrap();
+    engine
+        .create_window(
+            &flutter_engine::WindowArgs {
+                height: 1200,
+                width: 1800,
+                title: "Flutter App Demo",
+                mode: flutter_engine::WindowMode::Windowed,
+            },
+            assets_path.to_string_lossy().to_string(),
+            icu_data_path.to_string_lossy().to_string(),
+            vec![],
+        )
+        .unwrap();
+    engine.init_with_window_state(|window_state| {
+        window_state
+            .plugin_registrar
+            .add_plugin(calc_channel::CalcPlugin::default())
+            .add_plugin(dialog::DialogPlugin::default())
+            .add_plugin(msg_stream_channel::MsgStreamPlugin::default())
+            .add_plugin(window::WindowPlugin::default());
+    });
+    engine.run_window_loop(Some(&mut glfw_event_handler), None);
+}
 
-    let engine = FlutterEngine::new(args);
-    engine.add_plugin(Box::new(calc_channel::CalcPlugin::new()));
-    engine.add_plugin(Box::new(msg_stream_channel::MsgStreamPlugin::new()));
-    engine.run();
+fn glfw_event_handler(window_state: &mut DesktopWindowState, event: glfw::WindowEvent) -> bool {
+    match event {
+        glfw::WindowEvent::CursorPos(x, y) => {
+            let dragging = window_state.with_window_and_plugin_mut_result(
+                |window, p: &mut window::WindowPlugin| p.drag_window(window, x, y),
+            );
+            if let Some(dragging) = dragging {
+                !dragging
+            } else {
+                true
+            }
+        }
+        _ => true,
+    }
 }
--- rust/src/msg_stream_channel.rs
+++ rust/src/msg_stream_channel.rs
@@ -1,104 +1,83 @@
-use flutter_engine::{
-    Window,
-    PluginRegistry,
-    codec::{
-        MethodCallResult,
-        standard_codec::{
-            Value,
-        }
-    },
-    PlatformMessage,
-    FlutterEngineInner,
-    plugins::Plugin,
-    channel::{ Channel, StandardMethodChannel },
-};
-use std::{
-    iter::repeat,
-    sync::{ Arc, Mutex },
-    time::{ Duration },
-};
-use tokio::prelude::*;
-use stream_cancel::{StreamExt as StreamExt2, Tripwire, Trigger};
-use log::{info};
+use std::{iter::repeat, time::Duration};
 
+use flutter_engine::plugins::prelude::*;
+use log::info;
+use stream_cancel::{StreamExt as StreamExt2, Trigger, Tripwire};
+use tokio::prelude::*;
+
+const PLUGIN_NAME: &str = module_path!();
 const CHANNEL_NAME: &str = "rust/msg_stream";
 
 pub struct MsgStreamPlugin {
-    channel: Arc<Mutex<StandardMethodChannel>>,
-    stop_trigger: Option<Trigger>,
-}
-
-impl MsgStreamPlugin {
-    pub fn new() -> MsgStreamPlugin {
-        MsgStreamPlugin {
-            channel: Arc::new(Mutex::new(StandardMethodChannel::new(CHANNEL_NAME))),
-            stop_trigger: None,
-        }
-    }
+    channel: Weak<EventChannel>,
+    handler: Arc<RwLock<Handler>>,
 }
 
 impl Plugin for MsgStreamPlugin {
-    fn init_channel(&self, registry: &PluginRegistry) -> &str {
-        let channel = self.channel.lock().unwrap();
-        channel.init(registry);
-        CHANNEL_NAME
+    fn plugin_name() -> &'static str {
+        PLUGIN_NAME
     }
 
-    fn handle(&mut self, msg: &PlatformMessage, engine: Arc<FlutterEngineInner>, _window: &mut Window) {
-        let channel = self.channel.lock().unwrap();
-        let decoded = channel.decode_method_call(msg);
+    fn init_channels(&mut self, registrar: &mut ChannelRegistrar) {
+        let event_handler = Arc::downgrade(&self.handler);
+        self.channel = registrar.register_channel(EventChannel::new(CHANNEL_NAME, event_handler));
+    }
+}
 
-        info!("Got method call {}", decoded.method);
-        match decoded.method.as_str() {
-            "listen" => {
-                if let Value::I32(n) = decoded.args {
-                    info!("Random stream invoked with params {}", n);
-                }
-
-                channel.send_method_call_response(
-                    msg.response_handle,
-                    MethodCallResult::Ok(Value::Null)
-                );
-
-                let (trigger, tripwire) = Tripwire::new();
-                self.stop_trigger = Some(trigger);
-
-                let channel = self.channel.clone();
-                let e = engine.clone();
-                engine.with_async(|rt| {
-                    rt.spawn(futures::lazy(move || {
-                        let v = vec![
-                            "Hello?",
-                            "What's your name?",
-                            "How old are you?",
-                            "Maybe we can be friend together...",
-                            "Do you have a brother or sister?"
-                        ];
-                        stream::iter_ok::<_, ()>(repeat(v).flatten())
-                            .throttle(Duration::from_secs(1))
-                            .map_err(|e| eprintln!("Error = {:?}", e))
-                            .take_until(tripwire)
-                            .for_each(move |v| {
-                                let ret = Value::String(String::from(v));
-                                let channel = channel.clone();
-                                e.ui_thread(Box::new(move || {
-                                    let channel = channel.lock().unwrap();
-                                    channel.send_success_event(&ret);
-                                }));
-                                Ok(())
-                            })
-                    }));
-                });
-            },
-            "cancel" => {
-                // drop the trigger to stop stream
-                self.stop_trigger.take();
-                channel.send_method_call_response(
-                    msg.response_handle,
-                    MethodCallResult::Ok(Value::Null)
-                );
-            },
-            _ => (),
+impl Default for MsgStreamPlugin {
+    fn default() -> Self {
+        Self {
+            channel: Weak::new(),
+            handler: Arc::new(RwLock::new(Handler { stop_trigger: None })),
         }
+    }
+}
+
+struct Handler {
+    stop_trigger: Option<Trigger>,
+}
+
+impl EventHandler for Handler {
+    fn on_listen(
+        &mut self,
+        args: Value,
+        runtime_data: RuntimeData,
+    ) -> Result<Value, MethodCallError> {
+        if let Value::I32(n) = args {
+            info!("Random stream invoked with params {}", n);
+        }
+
+        let (trigger, tripwire) = Tripwire::new();
+        self.stop_trigger = Some(trigger);
+
+        let rt = runtime_data.clone();
+        runtime_data.task_executor.spawn(futures::lazy(move || {
+            let v = vec![
+                "Hello?",
+                "What's your name?",
+                "How old are you?",
+                "Maybe we can be friend together...",
+                "Do you have a brother or sister?",
+            ];
+            stream::iter_ok::<_, ()>(repeat(v).flatten())
+                .throttle(Duration::from_secs(1))
+                .map_err(|e| eprintln!("Error = {:?}", e))
+                .take_until(tripwire)
+                .for_each(move |v| {
+                    rt.with_channel(CHANNEL_NAME, move |channel| {
+                        let ret = Value::String(String::from(v));
+                        channel.send_success_event(&ret);
+                    })
+                    .unwrap();
+                    Ok(())
+                })
+        }));
+        Ok(Value::Null)
+    }
+
+    fn on_cancel(&mut self, _: RuntimeData) -> Result<Value, MethodCallError> {
+        // drop the trigger to stop stream
+        self.stop_trigger.take();
+        Ok(Value::Null)
     }
 }
